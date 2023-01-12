b11d3c8a5ee16ebe6accf6c7cd3a5cc1401bcb75
shinjuno123 <shinjuno123@naver.com>
2023-01-12 22:53:08 +0900

    Courses about spring security (login)

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/94/30bfdc35b991001d1bddea56bfeb4e3c
@@ -0,0 +1,11 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
---
+++ spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,60 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig {
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		
+		UserDetails john = User.builder()
+				.username("john").password("{noop}test123").roles("EMPLOYEE").build();
+		
+		UserDetails mary = User.builder()
+				.username("mary").password("{noop}test123").roles("MANAGER","EMPLOYEE").build();
+		
+		UserDetails susan = User.builder()
+				.username("susan").password("{noop}test123").roles("ADMIN","EMPLOYEE").build();
+		
+		return new InMemoryUserDetailsManager(john, mary, susan);
+	}
+	
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		return http.authorizeRequests(configurur->
+										configurur
+												.antMatchers("/").hasRole("EMPLOYEE")
+												.antMatchers("/leaders/**").hasRole("MANAGER")
+												.antMatchers("/systems/**").hasRole("ADMIN"))
+					
+				
+					.formLogin(configurer->
+								configurer
+										.loginPage("/showMyLoginPage")
+										.loginProcessingUrl("/authenticateTheUser")
+										.permitAll())
+					.logout(configurer->
+								configurer
+										.permitAll())
+					.exceptionHandling(configurer ->
+					configurer
+						.accessDeniedPage("/access-denied"))
+					
+					.build();
+	}
+	
+	
+
+}
